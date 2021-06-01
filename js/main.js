d077d051c3b0c7c5783449547a1a2e5c70aee3ea
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-01 21:43:37 +0300

    import js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+import gallaryItems from "./gallery-items.js";
+console.log(gallaryItems);
+
+function createItemCadrGallery(image) {}
+{
+  /* <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  >
+    <img
+      class="gallery__image"
+      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+      alt="Tulips"
+    />
+  </a>
+</li>; */
+}
